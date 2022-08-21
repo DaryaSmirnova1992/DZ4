ef08f97b3000ba49a4f572e78b9b9de314feff56
Darya <darecc@yandex.ru>
2022-08-21 16:37:37 +0400

    add Task_2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -5,9 +5,9 @@ int number2 = int.Parse(Console.ReadLine());
 Console.Write("Наибольшее значение: ");
 if(number1 > number2)
 {
-    Console.Write(number1);
+    Console.Write("максимальное "+ number1 + ", минимальное " + number2);
 }
 else
 {
-    Console.Write(number2);
+    Console.Write("максимальное "+ number2 + ", минимальное " + number1);
 }
