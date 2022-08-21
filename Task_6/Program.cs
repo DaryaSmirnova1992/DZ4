6643f3ddb6f35b6fb1b9c4a2d04b370eb33d5334
Darya <darecc@yandex.ru>
2022-08-21 17:01:38 +0400

    add Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+int even = number1 % 2;
+
+if(even == 0)
+{
+    Console.Write("Четное число");
+}
+else
+{Console.Write("Нечетное число");
+}
