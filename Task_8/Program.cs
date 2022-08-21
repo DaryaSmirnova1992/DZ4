3588ca5c094aff400a060972492ab10c5f1794ce
Darya <darecc@yandex.ru>
2022-08-21 17:36:01 +0400

    add Task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+int i = 2;
+while(i <= number1)
+{
+    Console.Write(i);
+    Console.Write(", ");
+    i = i + 2;
+}
