6c87e7578d7d7a9ad8a6e4fdee792f291d9ab9b8
Darya <darecc@yandex.ru>
2022-08-21 15:16:12 +0400

    add Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine ("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите третье число");
+int number3 = int.Parse(Console.ReadLine());
+Console.Write("Наибольшее значение: ");
+int max = number1;
+
+if(number1 > number2)
+{
+    max = number1;
+}
+if(number2 > number1)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+
+{
+    Console.Write(max);
+}
