2cf7808511b0ace76928093fffc9a103e126b16e
Darya <darecc@yandex.ru>
2022-08-21 14:55:07 +0400

    add gitignore

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Введите первое число");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Наибольшее значение: ");
+if(number1 > number2)
+{
+    Console.Write(number1);
+}
+else
+{
+    Console.Write(number2);
+}
